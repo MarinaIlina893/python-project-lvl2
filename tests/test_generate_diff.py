3a11ab6018241c0f1297a23e7283a9e98d9ac8a3
MarinaIlina893 <marinailina893@gmail.com>
2021-10-17 01:20:05 +0500

    add test coverage report

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,6 +1,6 @@
 from gendiff.generate_diff import generate_diff
 
-def test_happy_flow():
+def test_generate_diff():
     with open('tests/fixtures/generate_diff/result', 'r') as file:
         data = file.read()
     assert generate_diff('tests/fixtures/generate_diff/first.json', 'tests/fixtures/generate_diff/second.json') == data
